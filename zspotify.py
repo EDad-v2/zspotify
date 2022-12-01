c1c771e8c64465f8801076f3103749348b1f1283
Scarlett <scarlett@enby.site>
2022-12-01 13:14:08 -0500

    ffmpeg uses different flags for VBR and CBR

--- zspotify.py
+++ zspotify.py
@@ -56,7 +56,7 @@ ALBUM_IN_FILENAME = True # Puts album name in filename, otherwise name is (artis
 SKIP_EXISTING_FILES = True
 SKIP_PREVIOUSLY_DOWNLOADED = True
 MUSIC_FORMAT = os.getenv('MUSIC_FORMAT') or "mp3" # "mp3" | "ogg"
-USE_VBR = True # Encodes mp3 with variable bitrate, for smaller sizes. Does not affect ogg
+USE_VBR = False # Encodes mp3 with variable bitrate, for smaller sizes. Does not affect ogg
 FORCE_PREMIUM = False # set to True if not detecting your premium account automatically
 RAW_AUDIO_AS_IS = False # set to False if you wish you save the raw audio without re-encoding it.
 if os.getenv('RAW_AUDIO_AS_IS') != None and os.getenv('RAW_AUDIO_AS_IS') != "y":
@@ -630,7 +630,8 @@ def convert_audio_format(fromfilename, tofilename):
             bitrate = "0" if MUSIC_FORMAT == "mp3" and USE_VBR else "320k" # VBR 0 ~= 320kbps for MP3
         else:
             bitrate = "4" if MUSIC_FORMAT == "mp3" and USE_VBR else "160k" # VBR 4 ~= 160kbps for MP3
-        raw_audio.export(filename, format=MUSIC_FORMAT, parameters=["-q:a", bitrate])
+        bitrateFlag = "-q:a" if USE_VBR else "-b:a" # VBR and CBR use different ffmpeg flags
+        raw_audio.export(filename, format=MUSIC_FORMAT, parameters=[bitrateFlag, bitrate])
 
     else:
         '''Use ffmpeg-python to encode to mp3. If ogg, copy raw stream into ogg container.'''
